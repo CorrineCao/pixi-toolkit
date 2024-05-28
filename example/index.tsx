a08bacb118c25206a1e93c41e8a0a2c4863a8ef0
caochen <caochen1216@163.com>
2024-05-28 19:30:04 +0800

    example初始化展示

--- example/index.tsx
+++ example/index.tsx
@@ -1,15 +1,34 @@
 // import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
-// import { Thing } from '../.';
+import { PixiToolkit } from '../src/index';
 
 
-// console.log(">>>>Thing:", Thing);
+const appId = 'testApp';
 
 const App = () => {
+  React.useEffect(() => {
+
+    const initRenderApp = () => {
+        const canvasDom = document.getElementById(appId) as HTMLDivElement;
+        const mainApp = new PixiToolkit.MainApp({
+            domElement: canvasDom,
+        });
+        console.log(">>>mainApp", mainApp);
+      };
+      initRenderApp();
+}, []);
+
   return (
     <div>
-     test
+      <div
+          id={appId}
+          style={{
+            padding: 0,
+            margin: 0,
+            background: '#f3f4f7',
+          }}
+        />
     </div>
   );
 };
